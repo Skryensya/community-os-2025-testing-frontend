f9a67bedf7518ce2702e5d8a4710f1677bec6938
Allison <Allison.jpb@gmail.com>
2025-08-09 11:26:35 -0400

    implement vitest

--- tests/calculateDiscount.test.js
+++ tests/calculateDiscount.test.js
@@ -1,42 +1,30 @@
 // importa la función calculateDiscount usando ESM Modules
+import { describe, it, expect } from "vitest";
 import { calculateDiscount } from "../src/calculateDiscount.js";
 
-// Caso donde la diferencia es más de 15 días
-function testCase1() {
-  // ARRANGE: Declara variables daysDiff y expectedResult
-  const daysDiff = 20;
-  const expectedResult = 30;
-
-  // ACT: Ejecuta la función calculateDiscount con la variable daysDiff. guarda el resultado en result
-  const result = calculateDiscount(daysDiff);
-
-  // ASSERT
-  if (result === expectedResult) {
-    console.log("✅ Test Case 1 passed");
-  } else {
-    console.error(
-      `❌ Test Case 1 failed: expected ${expectedResult}, got ${result}`
-    );
-  }
-}
-
-// Caso donde la diferencia es entre 3 y 15 días
-function testCase2() {
-  //Implementa igual que en test case 1
-  const daysDiff = 8;
-  const expectedResult = 15;
-
-  const result = calculateDiscount(daysDiff);
-
-  if (result === expectedResult) {
-    console.log("✅ Test Case 2 passed");
-  } else {
-    console.error(
-      `❌ Test Case 2 failed: expected ${expectedResult}, got ${result}`
-    );
-  }
-}
-
-//Ejecución de casos
-testCase1();
-testCase2();
+describe("Pruebas de calculateDiscount", () => {
+  it("20 dayDiff => 30", () => {
+    const daysDiff = 20;
+    const result = calculateDiscount(daysDiff);
+    const expectedResult = 30;
+    expect(result).toBe(expectedResult);
+  });
+  it("8 dayDiff => 15", () => {
+    const daysDiff = 8;
+    const result = calculateDiscount(daysDiff);
+    const expectedResult = 15;
+    expect(result).toBe(expectedResult);
+  });
+  it("1 dayDiff => 0", () => {
+    const daysDiff = 1;
+    const result = calculateDiscount(daysDiff);
+    const expectedResult = 0;
+    expect(result).toBe(expectedResult);
+  });
+  it("-1 dayDiff => -1", () => {
+    const daysDiff = -1;
+    const result = calculateDiscount(daysDiff);
+    const expectedResult = -1;
+    expect(result).toBe(expectedResult);
+  });
+});
